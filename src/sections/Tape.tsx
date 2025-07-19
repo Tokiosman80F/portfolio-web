d0adc8d21fd91b51ef316e8f283843b8523f01dd
Tokiosman80F <tokiosman96@gmail.com>
2025-07-19 16:18:49 +0600

    style(tap-section): completed tap section styling

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,36 @@
+import StarIcon from "@/assets/icons/star.svg";
+
+const words = [
+  "Performant",
+  "Accessible",
+  "Secure",
+  "Interactive",
+  "Scaleable",
+  "User Friendly",
+  "Maintainable",
+  "Search Optimization",
+  "Usable",
+  "Reliable",
+];
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <div className=" py-8 | lg:py-16">
+      <div className="bg-gradient-to-r from-emerald-300 to-sky-400 overflow-x-clip -rotate-2 | lg:-rotate-1 ">
+        <div className="flex">
+          <div className="flex flex-none gap-4 py-4">
+            {words.map((word) => (
+              <div
+                key={word}
+                className=" inline-flex gap-4 items-center text-gray-900"
+              >
+                <span className="uppercase font-extrabold text-sm">{word}</span>{" "}
+                <StarIcon className="size-6 -rotate-4" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
