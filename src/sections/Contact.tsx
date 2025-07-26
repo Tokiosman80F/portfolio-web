74168fb95388c75e58968e474216df8156dd6962
unknown <tokiosman96@gmail.com>
2025-07-26 23:04:14 +0600

    style(footer):added bg-gradient at bottom

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -5,7 +5,7 @@ export const ContactSection = () => {
   return <div className="py-8 | md:py-28">
     <div className="container">
       <Card className="bg-gradient-to-r to-sky-300 from-emerald-400  outline-0">
-        <div className=" flex items-center gap-8 | sm:flex-col sm:justify-center | md:flex-row md:justify-around  ">
+        <div className=" flex items-center gap-8  flex-col justify-center | md:flex-row md:justify-around  ">
           <div>
           <h1 className="font-serif text-2xl text-gray-950 ">Let's create something amazing together </h1>
           <p className="text-sm max-w-lg mt-2 text-gray-800" >Ready to bring your next project to life ? Lets connect and discuss how I can help you achieve your your</p>        
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -6,13 +6,15 @@ const contactLinks = [
   { title: "Facebook", link: "#" },
 ];
 export const Footer = () => (
+  <footer className="relative overflow-x-clip">
+    <div className="-z-10 absolute h-[400px] w-[1600px] bg-emerald-300/40 bottom-0 left-1/2 -translate-x-1/2 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)]"   ></div>
   <div className="container">
     <div className="py-8 border-t-2 border-white/20 ">
-      <div className="flex flex-col items-center gap-8">
+      <div className="flex flex-col items-center gap-8 | md:flex-row md:justify-between">
         <p className="text-white/40 text-sm tracking-wider">
           &copy; {new Date().getFullYear()}. All rights reserved.
         </p>
-        <nav className="flex flex-col items-center gap-6 text-sm text-gray-100  ">
+        <nav className="flex flex-col items-center gap-6 text-sm text-gray-100 | md:flex-row ">
           {contactLinks.map((contact) => (
             <a
               href={contact.link}
@@ -27,4 +29,5 @@ export const Footer = () => (
       </div>
     </div>
   </div>
+  </footer>
 );
